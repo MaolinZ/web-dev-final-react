5ebe89bd4ee6c382b2069479038d8a55a1c4ca98
Maolin Zhang <maolinzq1@gmail.com>
2023-04-23 19:05:02 -0400

    Delete review admin bug

--- src/components/pages/search-results/details/review/review-list.tsx
+++ src/components/pages/search-results/details/review/review-list.tsx
@@ -3,11 +3,14 @@ import Review from "./review";
 
 
 
-export default function ReviewList(props: { reviews: ReviewProps[] }) {
+export default function ReviewList(props: { reviews: ReviewProps[], isAdmin?: boolean }) {
+
+    const {reviews, isAdmin} = props
+
     return (
         <div>
                 <div className={'review-list m-auto w-full'}>
-                    {props.reviews.map((r, i) => <Review review={r} key={i}/>)}
+                    {reviews.map((r, i) => <Review review={r} key={i} isAdmin={isAdmin}/>)}
                 </div>
         </div>
     );
