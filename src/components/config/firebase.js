8a35bd17bef75e29a6dbb97674876c0f1c47f679
Maolin Zhang <maolinzq1@gmail.com>
2023-04-23 14:55:34 -0400

    firebase config

--- src/components/config/firebase.js
+++ src/components/config/firebase.js
@@ -3,14 +3,10 @@ import { getFirestore } from "@firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-//const firebaseApp = require("firebase/app");
-//const firebaseAuth = require("firebase/auth");
-//const firebaseFirestore = require("firebase/firestore");
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = JSON.parse(process.env.REACT_APP_WEB_DEV_FINAL_FIREBASE);
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
