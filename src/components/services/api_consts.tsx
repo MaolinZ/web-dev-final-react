fb15b111edf5927242dc2270ee12444eb1dacbbd
Maolin Zhang <maolinzq1@gmail.com>
2023-04-23 22:16:30 -0400

    update api base const

--- src/components/services/api_consts.tsx
+++ src/components/services/api_consts.tsx
@@ -1,1 +1,2 @@
-export const API_BASE = process.env.API_BASE || "http://localhost:5000";
+// export const API_BASE = process.env.API_BASE || "http://localhost:5000";
+export const API_BASE = `https://web-dev-final-back-end.onrender.com`
